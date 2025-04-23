606ac6511dc612e556a79b4316401046d6de1402
Ruther Solloso <rfsolloso03@gmail.com>
2025-04-24 06:51:20 +0800

    database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'ruther',
-  database: 'test',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
