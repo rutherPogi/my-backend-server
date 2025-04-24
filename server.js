c1507f60d59be10f35165a4142412c6b077ee4a6
Ruther Solloso <rfsolloso03@gmail.com>
2025-04-24 08:21:56 +0800

    Updated Cors

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors()); // Allow cross-origin requests
+app.use(cors({
+  origin: 'https://e-tbayatmswdo.com',
+  credentials: true
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
